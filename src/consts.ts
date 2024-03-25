54fcf563df24cc81c3ce7e408074305fac977024
fl1po <igor@mitryakov.dev>
2024-03-25 18:17:31 +0400

    seo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'WARHOL';
+export const SITE_DESCRIPTION = 'WARHOL — серьезный молодежный журнал об актуальной музыке, визуальном искусстве и мировой культуре. ';
